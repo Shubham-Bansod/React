7885d1c62741ea15f38896c89ca4f012c827e185
Shubham-Bansod <shubhambansod474@gmail.com>
2021-12-24 20:16:39 +0530

    finished

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -21,8 +21,8 @@ const ExpenseForm = (props) => {
   const submitHandler = (event) => {
     event.preventDefault();
     const expenseData = {
-      title: { enteredTitle },
-      amount: { enteredAmount },
+      title: enteredTitle,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -30,7 +30,9 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
+    console.log(expenseData, "ed");
   };
+
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
@@ -64,6 +66,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
