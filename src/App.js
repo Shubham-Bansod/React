81f5a8349ab3dd11185ec514714f82cc1b82f89d
Shubham-Bansod <shubhambansod474@gmail.com>
2021-12-27 18:59:02 +0530

    userlist finished

--- src/App.js
+++ src/App.js
@@ -1,47 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
 
-import "./App.css";
-import CourseGoalList from "./components/CourseGoals/CourseGoalList/CourseGoalList";
-import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
-const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { text: "Do all exercises!", id: "g1" },
-    { text: "Finish the course!", id: "g2" },
-  ]);
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UserList";
 
-  const addGoalHandler = (enteredText) => {
-    setCourseGoals((prevGoals) => {
-      const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
-      return updatedGoals;
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserlist) => {
+      return [
+        ...prevUserlist,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
-
-  const deleteItemHandler = (goalId) => {
-    setCourseGoals((prevGoals) => {
-      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
-      return updatedGoals;
-    });
-  };
-
-  let content = (
-    <p style={{ textAlign: "center" }}>No Goals found. Maybe Add one?</p>
-  );
-
-  if (courseGoals.length > 0) {
-    content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
-    );
-  }
 
   return (
-    <div>
-      <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
-      </section>
-      <section id="goals">{content}</section>
-    </div>
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
-};
+}
 
 export default App;
